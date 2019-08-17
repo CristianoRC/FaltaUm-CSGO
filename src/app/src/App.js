44a4d5c283f57c7d266d1cca095e040e05e4853d
Gabriel Do Couto <gabrielcouto@Gabriels-MacBook-Pro.local>
2019-08-17 11:25:35 -0300

    Estrutura inicial do react

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Router, Switch, Route } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import { Home, Login } from './routes'
 
 function App() {
+  const history = createBrowserHistory()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/app/src/components/Button/Button.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Button = () => <button>show de bola</button>
+
+export default Button
